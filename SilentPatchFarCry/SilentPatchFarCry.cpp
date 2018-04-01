0417982d00f2d9128d426a1e1d1298fb05f87c4c
Silent <zdanio95@gmail.com>
2018-04-01 23:29:45 +0200

    Project setup

---
+++ SilentPatchFarCry/SilentPatchFarCry.cpp
@@ -0,0 +1,19 @@
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#define WINVER 0x0502
+#define _WIN32_WINNT 0x0502
+
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	UNREFERENCED_PARAMETER(hinstDLL);
+	UNREFERENCED_PARAMETER(lpvReserved);
+
+	if ( fdwReason == DLL_PROCESS_ATTACH )
+	{
+
+	}
+	return TRUE;
+}
